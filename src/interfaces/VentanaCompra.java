8986663348ddbd0765b444d74b02baa7ee9f0fcd
SebastianG03 <guamandavid11235@gmail.com>
2023-06-03 15:06:30 -0500

    Creación de la interfaz de compra y carro de compra

--- src/interfaces/VentanaCompra.java
+++ src/interfaces/VentanaCompra.java
@@ -5,5 +5,8 @@ import javax.swing.*;
 public class VentanaCompra {
     private JPanel panel1;
     private JTabbedPane tabbedPane1;
+    private JTabbedPane tabbedPane2;
     private JTable table1;
+    private JTable table2;
+    private JTable table3;
 }
